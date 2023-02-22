5a75ed0795c6c90e5dd7a0b37f566190aecc2ddc
yukino.xin <yukino.xin@foxmail.com>
2023-02-22 12:10:47 +0800

    mysql config

--- mysql/start.sh
+++ mysql/start.sh
@@ -1,10 +1,3 @@
-docker stop mysql;
-docker rm mysql;
-
-docker run \
-  -v /data/mysql/datadir:/var/lib/mysql \
-  -p 3306:3306 \
-  -e MYSQL_ROOT_PASSWORD=123456 \
-  --name mysql \
-  -d \
-  mysql:8.0.32
+docker compose stop;
+docker compose rm;
+docker compose up -d;
